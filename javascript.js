814a12100643a00c602a7dc0835758113f7c33c5
iamEvgen <evgen.alexa@gmail.com>
2022-05-20 15:07:57 +0300

    deep refactoring with EsLint (Air-bnb) and Prettier

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
-(function() {
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-use-before-define */
+/* eslint-disable comma-dangle */
+// eslint-disable-next-line func-names
+(function () {
   // VARIABLES
   let myLibrary = [];
   let idsCounter = 3;
@@ -16,12 +20,40 @@
   const inputReadOrNot = document.querySelector('#inputReadOrNot');
 
   class Book {
-    constructor(title, author, pages, inputReadOrNot, id) {
+    constructor(title, author, pages, readOrNot, id) {
       this.title = title;
       this.author = author;
       this.pages = pages;
-      this.readOrNot = inputReadOrNot;
+      this.readOrNot = readOrNot;
       this.id = id;
+    }
+
+    createCard() {
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.classList.add(`id-${this.id}`);
+      const title = document.createElement('div');
+      title.className = 'title';
+      title.textContent = this.title;
+      const author = document.createElement('div');
+      author.className = 'author';
+      author.textContent = this.author;
+      const pages = document.createElement('div');
+      pages.className = 'pages';
+      pages.textContent = `pages: ${this.pages}`;
+      const read = document.createElement('div');
+      read.className = 'read';
+      read.textContent = this.readOrNot;
+      setColor(read);
+      const delButton = document.createElement('button');
+      delButton.className = 'delButton';
+      delButton.textContent = 'Delete';
+      card.appendChild(title);
+      card.appendChild(author);
+      card.appendChild(pages);
+      card.appendChild(read);
+      card.appendChild(delButton);
+      return card;
     }
   }
 
@@ -31,10 +63,14 @@
     const addAuthor = inputAuthor.value;
     const addPages = inputPages.value;
     let addRead;
-    inputReadOrNot.checked ? addRead = '✓ Allready read' : addRead = '✖ Not read';
+    if (inputReadOrNot.checked) {
+      addRead = '✓ Allready read';
+    } else {
+      addRead = '✖ Not read';
+    }
     if (addTitle.length <= 300 && addAuthor.length <= 300 && addPages <= 2000) {
       const book = new Book(addTitle, addAuthor, addPages, addRead, idsCounter);
-      idsCounter++;
+      idsCounter += 1;
       myLibrary.push(book);
       renderCards();
       closePopupForm();
@@ -67,71 +103,65 @@
     modal.classList.add('opacity0');
   }
 
-  const book1 = new Book('Don Quixote', 'Miguel de Cervantes', '320', '✓ Allready read', 0);
+  const book1 = new Book(
+    'Don Quixote',
+    'Miguel de Cervantes',
+    '320',
+    '✓ Allready read',
+    0
+  );
   myLibrary.push(book1);
-  const book2 = new Book('Moby Dick', 'Herman Melville', '800', '✓ Allready read', 1);
+  const book2 = new Book(
+    'Moby Dick',
+    'Herman Melville',
+    '800',
+    '✓ Allready read',
+    1
+  );
   myLibrary.push(book2);
-  const book3 = new Book('War and Peace', 'Leo Tolstoy', '1300', '✖ Not read', 2);
+  const book3 = new Book(
+    'War and Peace',
+    'Leo Tolstoy',
+    '1300',
+    '✖ Not read',
+    2
+  );
   myLibrary.push(book3);
 
-  function createCard(book) {
-    const card = document.createElement('div');
-    card.className = 'card';
-    card.classList.add(`id-${book.id}`);
-    const title = document.createElement('div');
-    title.className = 'title';
-    title.textContent = book.title;
-    const author = document.createElement('div');
-    author.className = 'author';
-    author.textContent = book.author;
-    const pages = document.createElement('div');
-    pages.className = 'pages';
-    pages.textContent = `pages: ${book.pages}`;
-    const read = document.createElement('div');
-    read.className = 'read';
-    read.textContent = book.readOrNot;
-    setColor(read);
-    const delButton = document.createElement('button');
-    delButton.className = 'delButton';
-    delButton.textContent = 'Delete';
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(read);
-    card.appendChild(delButton);
-    insertCard(card);
-  }
-
-  function insertCard(card) {
-    cards.appendChild(card);
-  }
+  // function insertCard(card) {
+  //   cards.appendChild(card);
+  // }
 
   function prepareDelButtons() {
     delButtons = document.querySelectorAll('.delButton');
-    delButtons.forEach(element => {
+    delButtons.forEach((element) => {
       element.addEventListener('click', deleteCard);
     });
   }
 
   function prepareReadOrNotDivs() {
     readOrNotDivs = document.querySelectorAll('.read');
-    readOrNotDivs.forEach(element => {
+    readOrNotDivs.forEach((element) => {
       element.addEventListener('click', changeReadOrNot);
     });
   }
 
   function changeReadOrNot(event) {
-    const path = event.path || (event.composedPath && event.composedPath());
-    const id = path[1].className.split(' ')[1];
-    const fieldWithRead = document.querySelector(`.${id} .read`);
-    changeText(fieldWithRead);
-    setColor(fieldWithRead);
-  }
+    const path = event.composedPath && event.composedPath();
+    const id = path[1].className.split('-')[1];
+    for (const book of myLibrary) {
+      if (+book.id === +id) {
+        const readText = book.readOrNot;
+        if (readText === '✓ Allready read') {
+          book.readOrNot = '✖ Not read';
+        } else {
+          book.readOrNot = '✓ Allready read';
+        }
+      }
+    }
 
-  function changeText(fieldWithRead) {
-    fieldWithRead.textContent === '✓ Allready read' ? 
-    fieldWithRead.textContent = '✖ Not read' : 
-    fieldWithRead.textContent = '✓ Allready read';
+    // setColor(fieldWithRead);
+    renderCards();
   }
 
   function setColor(fieldWithRead) {
@@ -146,32 +176,29 @@
 
   function renderCards() {
     cards.innerHTML = '';
-    for (let book of myLibrary) {
-      createCard(book);
+    for (const book of myLibrary) {
+      const card = book.createCard();
+      cards.appendChild(card);
     }
     prepareDelButtons();
     prepareReadOrNotDivs();
   }
 
   function deleteCard(event) {
-    const path = event.path || (event.composedPath && event.composedPath());
+    const path = event.composedPath && event.composedPath();
     const idToDelete = path[1].classList[1].split('-')[1];
-    console.log(idToDelete);
-    myLibrary = myLibrary.filter((card) => {
-      return card.id != idToDelete;
-    });
+    myLibrary = myLibrary.filter((card) => card.id !== +idToDelete);
     renderCards();
   }
 
   // Event listeners
-  window.onclick = function(event) {
-    if (event.target == modal) {
+  window.addEventListener('click', (event) => {
+    if (event.target === modal) {
       closePopupForm();
     }
-  }
-
+  });
   addButton.addEventListener('click', () => openPopupForm());
-  modalBox.addEventListener('submit', addBookToLibrary)
+  modalBox.addEventListener('submit', addBookToLibrary);
 
   renderCards();
-})();
+}());
